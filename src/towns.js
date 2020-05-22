e8edf50f6f3a33c032583bf9361e244ef4dc2a9e
tomilova-nastya <nastya.aisha@gmail.com>
2020-05-22 18:13:18 +0300

    более правильный путь к index.js

--- src/towns.js
+++ src/towns.js
@@ -36,7 +36,7 @@ const homeworkContainer = document.querySelector('#homework-container');
  Массив городов пожно получить отправив асинхронный запрос по адресу
  https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json
  */
-import { loadAndSortTowns as loadTowns } from '../src/index.js';
+import { loadAndSortTowns as loadTowns } from './index';
 
 /*
  Функция должна проверять встречается ли подстрока chunk в строке full
