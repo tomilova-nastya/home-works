830a6a9800ac411c6948be6f81030a1a01b1ef0e
tomilova-nastya <nastya.aisha@gmail.com>
2020-05-30 19:11:30 +0300

    попытка что-то сделать

--- src/index.js
+++ src/index.js
@@ -1,26 +1,7 @@
-//import { mapInit } from './js/ymaps';
-import render from './templates/friends.hbs';
+import {mapInit, openPopup} from './js/ymaps';
+//import render from './templates/friends.hbs';
 
-//window.onload = mapInit()
+mapInit();
 
 const map = document.querySelector('#map');
 
-const list = [
-  {
-    name: 'Иван', last_name: 'Иванов'
-  },
-  {
-    name: 'Олег', last_name: 'Петров'
-  },
-  {
-    name: 'Игорь', last_name: 'Авдеев'
-  },
-  {
-    name: 'Денис', last_name: 'Иванов'
-  },
-  {
-    name: 'Артем', last_name: 'Картушин'
-  },
-];
-
-map.innerHTML = render ({ list })
--- src/js/ymaps.js
+++ src/js/ymaps.js
@@ -1,12 +1,50 @@
 function mapInit() {
-  ymaps.ready(() => {
-    let moscow_map = new ymaps.Map('map', {
-      center: [55.7, 37.6],
-      zoom: 10
+    ymaps.ready(() => {
+        let myMap = new ymaps.Map('map', {
+            center: [55.7, 37.6],
+            zoom: 10
+        });
+
+        myMap.events.add('click', function(e) {
+            var coords = e.get('coords');
+            var geoCoords = ymaps.geocode(coords);
+            var position = e.get('position');
+
+            geoCoords.then(response => {
+                var obj = {};
+                obj.coords = coords;
+                obj.address = response.geoObjects.get(0).properties.get('text');
+                obj.comments = [];
+                openPopup(obj, response.geoObjects);
+            });
+        });
     })
-  })
+}
+
+function openPopup(obj, geoObjects) {
+    var myPlaceMark = new ymaps.GeoObject({
+        geometry: {
+            type: "Point",
+            coordinates: [55.75, 37.63]
+        },
+        properties: {
+            // Контент метки.
+            iconContent: 'icon контент',
+            hintContent: 'hint контент'
+        }
+    }, {});
+
+    geoObjects
+        .add(myPlaceMark)
+        .add(new ymaps.Placemark(obj.coords, {
+            balloonContent: 'цвет <strong>воды пляжа бонди</strong>'
+        }, {
+            preset: 'islands#icon',
+            iconColor: '#0095b6'
+        }));
 }
 
 export {
-  mapInit
+    mapInit,
+    openPopup
 }
