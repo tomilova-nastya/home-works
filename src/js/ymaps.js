5632e836afcfa46edeb7898dc0896fc69716058a
tomilova-nastya <nastya.aisha@gmail.com>
2020-06-04 01:50:07 +0300

    карусель - начало

--- src/js/ymaps.js
+++ src/js/ymaps.js
@@ -13,126 +13,146 @@ function mapInit() {
             zoom: 10
         });
 
-        getExistingBalloons().then(() => {
-            var clusterer = new ymaps.Clusterer({
-                preset: 'islands#invertedDarkOrangeClusterIcons',
-                clusterDisableClickZoom: true,
-                openBalloonOnClick: true,
-                groupByCoordinates: false,
-                clusterBalloonContentLayout: 'cluster#balloonCarousel'
-            });
-            myMap.geoObjects.add(clusterer);
+        getExistingBalloons()
+            .then(() => {
+                let balloonHTML = balloonRender([]);
+                let customItemContentLayout = ymaps.templateLayoutFactory.createClass(
+                    balloonHTML, {
+                        build: function () {
+                            customItemContentLayout.superclass.build.call(this);
+                            document.querySelector('.ballon_header').addEventListener('click', this.onClick);
+                        },
+                        onClick: function () {
+                            //let adress = document.querySelector('.baloon__adress').innerHTML;
+                            // let filterComments = comments.filter(item => {
+                            //     return (item.adress === adress);
+                            // });
+                            document.querySelector('.ballon_header').innerHTML = 'WOW'; //рендер комментариев в абстрактный блок формы listComments, как показывал рендер на созвоне, но можешь сделать иначе)
 
-            myMap.events.add('click', function (event) {
-                let coords = event.get('coords');
-                //let position = e.get('position');
+                        }
+                    });
 
-                getActualBalloonContent(coords).then((balloonContent) => {
-                    createPlacemarkAndOpenBalloon(myMap, balloonContent, clusterer);
+                var clusterer = new ymaps.Clusterer({
+                    preset: 'islands#invertedDarkOrangeClusterIcons',
+                    clusterDisableClickZoom: true,
+                    openBalloonOnClick: true,
+                    groupByCoordinates: false,
+                    clusterBalloonContentLayout: 'cluster#balloonCarousel',
+                    clusterBalloonItemContentLayout: customItemContentLayout,
+                    clusterBalloonContentWidth: 448,
+                    clusterBalloonContentLayoutWidth: 448,
+                    clusterBalloonContentLayoutHeight: 400,
+                });
+                myMap.geoObjects.add(clusterer);
+
+                myMap.events.add('click', function (event) {
+                    let coords = event.get('coords');
+                    //let position = e.get('position');
+
+                    getActualBalloonContent(coords).then((balloonContent) => {
+                        createPlacemarkAndOpenBalloon(myMap, balloonContent, clusterer);
+                    });
                 });
             });
-        });
 
     })
 }
 
-let simpleText = 'simpapuska';
+// function clasterizator(myMap, allExistingBalloons) {
+//     let clusterer = new ymaps.Clusterer({
+//         preset: 'islands#invertedVioletClusterIcons',
+//         groupByCoordinates: false,
+//         clusterDisableClickZoom: true,
+//         clusterHideIconOnBalloonOpen: false,
+//         geoObjectHideIconOnBalloonOpen: false
+//     });
+//     /**
+//      * Функция возвращает объект, содержащий данные метки.
+//      * Поле данных clusterCaption будет отображено в списке геообъектов в балуне кластера.
+//      * Поле balloonContentBody - источник данных для контента балуна.
+//      * Оба поля поддерживают HTML-разметку.
+//      * Список полей данных, которые используют стандартные макеты содержимого иконки метки
+//      * и балуна геообъектов, можно посмотреть в документации.
+//      * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/GeoObject.xml
+//      */
+//     let getPointData = function (pointData) {
+//         let balloonContent = getBalloonContent(allExistingBalloons, pointData);
+//         let balloonHTML = balloonRender(balloonContent);
+//
+//         if (balloonContent !== undefined) {
+//             return {
+//                 balloonContentHeader: '',
+//                 balloonContentBody: balloonHTML,
+//                 balloonContentFooter: '',
+//                 clusterCaption: '<p class="balloon_address" onclick="balloonHandler()">' + balloonContent.address + '</></p>'
+//             };
+//         } else {
+//             return {
+//                 balloonContentHeader: '',
+//                 balloonContentBody: balloonHTML,
+//                 balloonContentFooter: '',
+//                 clusterCaption: '<p class="balloon_address" onclick="balloonHandler()">EmptyAddress</p>'
+//             };
+//         }
+//
+//     }
+//
+//     /**
+//      * Функция возвращает объект, содержащий опции метки.
+//      * Все опции, которые поддерживают геообъекты, можно посмотреть в документации.
+//      * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/GeoObject.xml
+//      */
+//     let getPointOptions = function () {
+//         return {
+//             preset: 'islands#violetIcon'
+//         };
+//     };
+//
+//     let points = getExistingPoints();
+//
+//     if (points.length === 0) {
+//         return;
+//     }
+//
+//     let geoObjects = [];
+//
+//     /**
+//      * Данные передаются вторым параметром в конструктор метки, опции - третьим.
+//      * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/Placemark.xml#constructor-summary
+//      */
+//     for (var i = 0, len = points.length; i < len; i++) {
+//         geoObjects[i] = new ymaps.Placemark(points[i], getPointData(points[i]), getPointOptions());
+//         myGeoObjects[i] = geoObjects[i];
+//     }
+//
+//     /**
+//      * Можно менять опции кластеризатора после создания.
+//      */
+//     clusterer.options.set({
+//         gridSize: 80,
+//         clusterDisableClickZoom: true
+//     });
+//
+//     /**
+//      * В кластеризатор можно добавить javascript-массив меток (не геоколлекцию) или одну метку.
+//      * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/Clusterer.xml#add
+//      */
+//     clusterer.add(geoObjects);
+//     myMap.geoObjects.add(clusterer);
+//
+//     /**
+//      * Спозиционируем карту так, чтобы на ней были видны все объекты.
+//      */
+//
+//     myMap.setBounds(clusterer.getBounds(), {
+//         checkZoomRange: true
+//     });
+// }
 
-function clasterizator(myMap, allExistingBalloons) {
-    let clusterer = new ymaps.Clusterer({
-        preset: 'islands#invertedVioletClusterIcons',
-        groupByCoordinates: false,
-        clusterDisableClickZoom: true,
-        clusterHideIconOnBalloonOpen: false,
-        geoObjectHideIconOnBalloonOpen: false
-    });
-    /**
-     * Функция возвращает объект, содержащий данные метки.
-     * Поле данных clusterCaption будет отображено в списке геообъектов в балуне кластера.
-     * Поле balloonContentBody - источник данных для контента балуна.
-     * Оба поля поддерживают HTML-разметку.
-     * Список полей данных, которые используют стандартные макеты содержимого иконки метки
-     * и балуна геообъектов, можно посмотреть в документации.
-     * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/GeoObject.xml
-     */
-    let getPointData = function (pointData) {
-        let balloonContent = getBalloonContent(allExistingBalloons, pointData);
-        let balloonHTML = balloonRender(balloonContent);
-
-        if (balloonContent !== undefined) {
-            return {
-                balloonContentHeader: '',
-                balloonContentBody: balloonHTML,
-                balloonContentFooter: '',
-                clusterCaption: '<p class="balloon_address" onclick="balloonHandler()">' + balloonContent.address + '</></p>'
-            };
-        } else {
-            return {
-                balloonContentHeader: '',
-                balloonContentBody: balloonHTML,
-                balloonContentFooter: '',
-                clusterCaption: '<p class="balloon_address" onclick="balloonHandler()">EmptyAddress</p>'
-            };
-        }
-
-    }
-
-    /**
-     * Функция возвращает объект, содержащий опции метки.
-     * Все опции, которые поддерживают геообъекты, можно посмотреть в документации.
-     * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/GeoObject.xml
-     */
-    let getPointOptions = function () {
-        return {
-            preset: 'islands#violetIcon'
-        };
-    };
-
-    let points = getExistingPoints();
-
-    if (points.length === 0) {
-        return;
-    }
-
-    let geoObjects = [];
-
-    /**
-     * Данные передаются вторым параметром в конструктор метки, опции - третьим.
-     * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/Placemark.xml#constructor-summary
-     */
-    for (var i = 0, len = points.length; i < len; i++) {
-        geoObjects[i] = new ymaps.Placemark(points[i], getPointData(points[i]), getPointOptions());
-        myGeoObjects[i] = geoObjects[i];
-    }
-
-    /**
-     * Можно менять опции кластеризатора после создания.
-     */
-    clusterer.options.set({
-        gridSize: 80,
-        clusterDisableClickZoom: true
-    });
-
-    /**
-     * В кластеризатор можно добавить javascript-массив меток (не геоколлекцию) или одну метку.
-     * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/Clusterer.xml#add
-     */
-    clusterer.add(geoObjects);
-    myMap.geoObjects.add(clusterer);
-
-    /**
-     * Спозиционируем карту так, чтобы на ней были видны все объекты.
-     */
-
-    myMap.setBounds(clusterer.getBounds(), {
-        checkZoomRange: true
-    });
-}
-
-function openBalloon(coords) {
-    //найти нужную метку
-    console.log(myGeoObjects);
-}
+// function openBalloon(coords) {
+//     //найти нужную метку
+//     console.log(myGeoObjects);
+// }
 
 function getActualBalloonContent(coords) {
     return ymaps.geocode(coords).then(response => {
@@ -146,24 +166,24 @@ function getActualBalloonContent(coords) {
     });
 }
 
-function generateBalloonContent(placemarkData) {
-    return ymaps.geocode(placemarkData.coords).then(response => {
-        let balloonContent = {};
-
-        balloonContent.coords = placemarkData.coords;
-        balloonContent.address = response.geoObjects.get(0).properties.get('text');
-        balloonContent.comments = placemarkData.comments;
-
-        return balloonContent;
-    });
-}
+// function generateBalloonContent(placemarkData) {
+//     return ymaps.geocode(placemarkData.coords).then(response => {
+//         let balloonContent = {};
+//
+//         balloonContent.coords = placemarkData.coords;
+//         balloonContent.address = response.geoObjects.get(0).properties.get('text');
+//         balloonContent.comments = placemarkData.comments;
+//
+//         return balloonContent;
+//     });
+// }
 
 function getExistingBalloons() {
     return new Promise((resolve) => {
         let existingBalloonContent = [];
 
         if (allReviewsDictionary.length > 0) {
-            for (let i = 0; i < allReviewsDictionary.length ; i++) {
+            for (let i = 0; i < allReviewsDictionary.length; i++) {
                 ymaps.geocode(allReviewsDictionary[i].coords).then((response) => {
                     existingBalloonContent.push({
                         coords: allReviewsDictionary[i].coords,
@@ -183,6 +203,10 @@ function getExistingBalloons() {
 }
 
 function createPlacemarkAndOpenBalloon(myMap, balloonContent, clasterer) {
+    let balloonHTML = balloonRender(balloonContent);
+
+
+
     let myPlacemark = new ymaps.Placemark(balloonContent.coords, {
         balloonContent: balloonRender(balloonContent)
     }, {
@@ -234,54 +258,54 @@ function createPlacemarkAndOpenBalloon(myMap, balloonContent, clasterer) {
     myPlacemark.balloon.open();
 }
 
-function createPlacemarkAndBalloon(myMap, baloonContent) {
-    let myPlacemark = new ymaps.Placemark(baloonContent.coords, {
-        balloonContent: balloonRender(baloonContent)
-    }, {
-        preset: 'islands#icon',
-        iconColor: '#0095b6'
-    })
+// function createPlacemarkAndBalloon(myMap, baloonContent) {
+//     let myPlacemark = new ymaps.Placemark(baloonContent.coords, {
+//         balloonContent: balloonRender(baloonContent)
+//     }, {
+//         preset: 'islands#icon',
+//         iconColor: '#0095b6'
+//     })
+//
+//     myMap.geoObjects.add(myPlacemark);
+//
+//     myPlacemark.balloon.events.add('open', function (event) {
+//         let sendButton = document.querySelector('input.sendButton');
+//         let nameInput = document.querySelector('input.nameInput');
+//         let placeInput = document.querySelector('input.placeInput');
+//         let commentInput = document.querySelector('textarea.commentTextarea');
+//
+//         let date = new Date();
+//         let currentDate = `${date.getDay()}.${date.getMonth()}.${date.getFullYear()}`;
+//
+//         sendButton.addEventListener('click', (e) => {
+//             let coords = baloonContent.coords;
+//             let newReview = {
+//                 name: nameInput.value,
+//                 place: placeInput.value,
+//                 comment: commentInput.value,
+//                 date: currentDate
+//             }
+//
+//             addOrUpdateReview(coords, newReview);
+//
+//             myPlacemark.balloon.close();
+//         })
+//     });
+// }
 
-    myMap.geoObjects.add(myPlacemark);
-
-    myPlacemark.balloon.events.add('open', function (event) {
-        let sendButton = document.querySelector('input.sendButton');
-        let nameInput = document.querySelector('input.nameInput');
-        let placeInput = document.querySelector('input.placeInput');
-        let commentInput = document.querySelector('textarea.commentTextarea');
-
-        let date = new Date();
-        let currentDate = `${date.getDay()}.${date.getMonth()}.${date.getFullYear()}`;
-
-        sendButton.addEventListener('click', (e) => {
-            let coords = baloonContent.coords;
-            let newReview = {
-                name: nameInput.value,
-                place: placeInput.value,
-                comment: commentInput.value,
-                date: currentDate
-            }
-
-            addOrUpdateReview(coords, newReview);
-
-            myPlacemark.balloon.close();
-        })
-    });
-}
-
-function createPlacemark(myMap, balloonContent, clasterer) {
-    let myPlacemark = new ymaps.Placemark(balloonContent.coords, {
-        balloonContent: balloonRender(balloonContent)
-    }, {
-        preset: 'islands#icon',
-        iconColor: '#0095b6'
-    })
-
-    //myMap.geoObjects.add(myPlacemark);
-    clasterer.add(myPlacemark);
-
-    return myPlacemark;
-}
+// function createPlacemark(myMap, balloonContent, clasterer) {
+//     let myPlacemark = new ymaps.Placemark(balloonContent.coords, {
+//         balloonContent: balloonRender(balloonContent)
+//     }, {
+//         preset: 'islands#icon',
+//         iconColor: '#0095b6'
+//     })
+//
+//     //myMap.geoObjects.add(myPlacemark);
+//     clasterer.add(myPlacemark);
+//
+//     return myPlacemark;
+// }
 
 function addOrUpdateReview(coords, newReview) {
     let isExist = false;
@@ -299,7 +323,6 @@ function addOrUpdateReview(coords, newReview) {
             value: [newReview]
         })
     }
-
 
 
     // let existingReviews = getExistingReviews(coords);
@@ -329,48 +352,48 @@ function getExistingReviews(coords) {
     return [];
 }
 
-function getBalloonContent(allExistingBalloons, coords) {
-    for (let i = 0; i < allExistingBalloons.length; i++) {
-        let existingCoords = `[${allExistingBalloons[i].coords[0]},${allExistingBalloons[i].coords[1]}]`;
-        let currentCoords = `[${coords[0]},${coords[1]}]`;
-
-        if (existingCoords === currentCoords) {
-            return allExistingBalloons[i];
-        }
-    }
-}
-
-function getExistingPlaces() {
-    let existingPlaces = [];
-
-    for (let i = 0; i < localStorage.length; i++) {
-        let coords_string = localStorage.key(i);
-        let comments_string = localStorage.getItem(coords_string);
-
-        if (comments_string !== 'INFO') {
-            existingPlaces.push({
-                coords: JSON.parse(coords_string),
-                comments: JSON.parse(comments_string)
-            })
-        }
-    }
-
-    return existingPlaces;
-}
-
-function getExistingPoints() {
-    let existingPoints = [];
-
-    for (let i = 0; i < localStorage.length; i++) {
-        let coords_string = localStorage.key(i);
-
-        if (coords_string !== 'loglevel:webpack-dev-server') {
-            existingPoints.push(JSON.parse(coords_string));
-        }
-    }
-
-    return existingPoints;
-}
+// function getBalloonContent(allExistingBalloons, coords) {
+//     for (let i = 0; i < allExistingBalloons.length; i++) {
+//         let existingCoords = `[${allExistingBalloons[i].coords[0]},${allExistingBalloons[i].coords[1]}]`;
+//         let currentCoords = `[${coords[0]},${coords[1]}]`;
+//
+//         if (existingCoords === currentCoords) {
+//             return allExistingBalloons[i];
+//         }
+//     }
+// }
+//
+// function getExistingPlaces() {
+//     let existingPlaces = [];
+//
+//     for (let i = 0; i < localStorage.length; i++) {
+//         let coords_string = localStorage.key(i);
+//         let comments_string = localStorage.getItem(coords_string);
+//
+//         if (comments_string !== 'INFO') {
+//             existingPlaces.push({
+//                 coords: JSON.parse(coords_string),
+//                 comments: JSON.parse(comments_string)
+//             })
+//         }
+//     }
+//
+//     return existingPlaces;
+// }
+//
+// function getExistingPoints() {
+//     let existingPoints = [];
+//
+//     for (let i = 0; i < localStorage.length; i++) {
+//         let coords_string = localStorage.key(i);
+//
+//         if (coords_string !== 'loglevel:webpack-dev-server') {
+//             existingPoints.push(JSON.parse(coords_string));
+//         }
+//     }
+//
+//     return existingPoints;
+// }
 
 export {
     mapInit
