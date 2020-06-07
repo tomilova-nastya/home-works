e42cdffe31272bb601e625692360e57609210503
tomilova-nastya <nastya.aisha@gmail.com>
2020-06-08 02:27:40 +0300

    работает логин + переход на форму чата

--- app.js
+++ app.js
@@ -1,30 +1,46 @@
-var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+let express = require("express");
+let app = express();
+app.use(express.static('dist'));
+
+let http = require('http').createServer(app);
+let io = require('socket.io')(http);
+
 
 app.get('/', (req, res) => {
-    res.sendfile(__dirname + '/dist/index.html');
 });
+
+
+let globalMessages = [];
+let participants = [];
+let activeParticipantsCount = 0;
+
+io.on('connection', function(socket) {
+
+    socket.on('participantLogin', (loginString) => {
+        let loginData = loginString.split(';');
+        participants.push(newParticipant(loginData[0], loginData[1], '', ''));
+        activeParticipantsCount++;
+
+        socket.emit('getParticipants', { description: JSON.stringify(participants) });
+        socket.emit('getActiveParticipantsCount', { description: `${activeParticipantsCount}` });
+    });
+
+    socket.on('disconnect', function () {
+        //activeParticipantsCount--;
+    });
+});
+
 
 let port = 8080;
 http.listen(port, function() {
     console.log('listening on *:' + port);
 });
 
-//Whenever someone connects this gets executed
-io.on('connection', function(socket) {
-    console.log('A user connected');
-
-    //Whenever someone disconnects this piece of code executed
-    socket.on('disconnect', function () {
-        console.log('A user disconnected');
-    });
-});
-
-io.on('exit', function () {
-    http.close();
-});
-
-io.on('uncaughtException', function (){
-    io.on('SIGTERM', 'kill');
-})
+function newParticipant(name, nickname, photoUrl, lastMessage){
+    return {
+        name: name,
+        nickname: nickname,
+        photoUrl: photoUrl,
+        lastMessage: lastMessage
+    };
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,47 @@
-import startPageRender from '../index.hbs';
+import './styles.css';
 import loginFormRender from './templates/login-form.hbs';
 import chatFormRender from './templates/chat-form.hbs';
+import messagesRender from './templates/messages.hbs';
+import participantsRender from './templates/participants.hbs';
 
+
+let socket = io();
 
 function chatInit() {
-  const container = document.querySelector('.container');
-  container.innerHTML = loginFormRender ({});
+    const container = document.querySelector('.container');
+    container.innerHTML = loginFormRender({});
+
+    const nameInput = document.querySelector('.login-form__nameInput');
+    const nicknameInput = document.querySelector('.login-form__nicknameInput');
+    const sendButton = document.querySelector('.login-form__sendButton');
+
+    sendButton.addEventListener('click', () => {
+        socket.emit('participantLogin', `${nameInput.value};${nicknameInput.value}`);
+
+        let participants = [];
+        let activeParticipantsCount = 0;
+
+        socket.on('getParticipants', (response) => {
+            participants = JSON.parse(response.description);
+
+            socket.on('getActiveParticipantsCount', (response) => {
+                activeParticipantsCount = Number(response.description);
+
+                container.innerHTML = chatFormRender({});
+
+                let participantsBlock = document.querySelector('.participants-block__wrapper');
+                let messagesBlock = document.querySelector('.messages-block__wrapper');
+
+                // console.log(participants[0].name);
+                // console.log(participants[0].photoUrl);
+                // console.log(participants[0].lastMessage);
+
+                participantsBlock.innerHTML = participantsRender({ participants: participants });
+                messagesBlock.innerHTML = messagesRender({ activeParticipantsCount: activeParticipantsCount, messages: [] });
+            });
+        });
+    });
 }
 
-console.log("Что-то получается");
 
 chatInit();
